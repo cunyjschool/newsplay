a8f8aab172156bf21bbfd6a7d80932f10d96b1df
Jue Yang <jueyang22@gmail.com>
2016-08-08 17:02:25 -0400

    add built script and assets

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -21419,8 +21419,50 @@ module.exports = Zepto
 })(Zepto)
 
 },{}],158:[function(require,module,exports){
-var article = require('./build/article.js');
-var attributes = require('./build/attributes.js');
+var article = require("./build/article.js");
+var attributes = require("./build/attributes.js");
+
+function startIntro(){
+  var intro = introJs();
+      intro.setOptions({
+        steps: [
+          {
+            intro: "Hi! Welcome to <strong>NewsPlay</strong>, a game to generate creative approaches to covering news stories. Here's how to play."
+          },
+          {
+            element: "#headline",
+            intro: "You have the summary of (and link to) a randomly-selected news story from NY Times, sourced from the <a href='http://developer.nytimes.com/article_search_v2.json' target='_blank'>NYT Article Search API</a>.",
+            position: "right"
+          },
+          {
+            element: "#criteria",
+            intro: "You also have a new set of constraints to cover this story differently, including a new story type, supplemental media type, etc.",
+            position: "left"
+          },
+          {
+            intro: "Use the next 5 minutes to develop an approach to that story that you could pitch to a publication editor for a publication of your choice."
+          },
+          {
+            intro:"Draft a 3- to 5-sentence pitch. If you feel inspired by the supplemental media type or the platform, add your ideas for additional social media content in your pitch."
+          },
+          {
+            element: "#submit",
+            intro: "Submit your approach and see other's ideas.",
+            position: "top"
+          },
+          {
+            element: "#generate",
+            intro: "Generate a fresh combination if you are stuck!",
+            position: "left"
+          }
+        ]
+      });
+
+  intro.start();
+};
+
+document.getElementById("help").onclick = function(){startIntro();};
+document.getElementById("generate").onclick= function(){window.location.reload();};
 
 },{"./build/article.js":160,"./build/attributes.js":161}],159:[function(require,module,exports){
 module.exports=[{
@@ -21444,7 +21486,7 @@ module.exports=[{
     "color":"#B4436C"
 },
 {
-    "name": "Audience Age",
+    "name": "Audience Age (optional)",
     "options":["18 and under","twentysomethings","30+","seniors"],
     "color":"#995FA3"
 }]

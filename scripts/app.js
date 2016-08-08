1146228279085bd90e65218681ad86724041c389
Jue Yang <jueyang22@gmail.com>
2016-08-08 10:28:33 -0400

    move introjs to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,43 @@
-var article = require('./build/article.js');
-var attributes = require('./build/attributes.js');
+var article = require("./build/article.js");
+var attributes = require("./build/attributes.js");
+
+function startIntro(){
+  var intro = introJs();
+      intro.setOptions({
+        steps: [
+          {
+            intro: "Hi! Welcome to <strong>NewsPlay</strong>, a game to generate creative approaches to covering news stories. Here's how to play."
+          },
+          {
+            element: "#headline",
+            intro: "You have the summary of (and link to) a randomly-selected news story from NY Times, sourced from the <a href='http://developer.nytimes.com/article_search_v2.json' target='_blank'>NYT Article Search API</a>.",
+            position: "right"
+          },
+          {
+            element: "#criteria",
+            intro: "Look at the constraints on this side for a suggested story type, a supplemental media type, etc.",
+            position: "left"
+          },
+          {
+            intro: "Use the next 5 minutes to develop an approach to that story that you could pitch to a publication editor for a publication of your choice."
+          },
+          {
+            intro:"Draft a 3- to 5-sentence pitch. If you feel inspired by the supplemental media type or the platform, add your ideas for additional social media content in your pitch."
+          },
+          {
+            element: "#submit",
+            intro: "Submit your approach and see other's ideas.",
+            position: "top"
+          },
+          {
+            element: "#generate",
+            intro: "For a fresh combination, generate a new story and options.",
+            position: "left"
+          }
+        ]
+      });
+
+  intro.start();
+};
+
+document.getElementById("help").onclick = function(){startIntro();};
